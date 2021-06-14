5a1fd1008c3af03e428f730f995a0ab34bd60b33
Mirco Kraenz <kraenzmirco@gmail.com>
2021-06-14 10:03:26 +0200

    CHANGE allow listen-only mode i.e. chatbot does not _write_ to chat itself

--- src/ttvchat/ChatClient.ts
+++ src/ttvchat/ChatClient.ts
@@ -1,17 +1,17 @@
+import { Badges, ChatUserstate, Client, Options } from "tmi.js";
 import {
-  Badges, ChatUserstate, Client,
-  Options
-} from "tmi.js";
-import {
-  ConfigurationChangeEvent, Disposable, Event, EventEmitter,
-  ExtensionContext, workspace,
-  WorkspaceConfiguration
-} from 'vscode';
+  ConfigurationChangeEvent,
+  Disposable,
+  Event,
+  EventEmitter,
+  ExtensionContext,
+  workspace,
+  WorkspaceConfiguration,
+} from "vscode";
 import { keytar } from "../common";
 import { Configuration, KeytarKeys, LogLevel, Settings } from "../enums";
-import { log } from '../logger';
-import { API } from './api';
-
+import { log } from "../logger";
+import { API } from "./api";
 
 interface IBadges extends Badges {
   [key: string]: string | undefined;
@@ -24,11 +24,15 @@ export interface ChatClientMessageReceivedEvent {
 }
 
 export class ChatClient implements Disposable {
-  private readonly _onChatClientConnected: EventEmitter<boolean> = new EventEmitter();
-  private readonly _onChatClientMessageReceived: EventEmitter<ChatClientMessageReceivedEvent> = new EventEmitter();
+  private readonly _onChatClientConnected: EventEmitter<boolean> =
+    new EventEmitter();
+  private readonly _onChatClientMessageReceived: EventEmitter<ChatClientMessageReceivedEvent> =
+    new EventEmitter();
 
-  public readonly onChatClientConnected: Event<boolean> = this._onChatClientConnected.event;
-  public readonly onChatClientMessageReceived: Event<ChatClientMessageReceivedEvent> = this._onChatClientMessageReceived.event;
+  public readonly onChatClientConnected: Event<boolean> =
+    this._onChatClientConnected.event;
+  public readonly onChatClientMessageReceived: Event<ChatClientMessageReceivedEvent> =
+    this._onChatClientMessageReceived.event;
 
   private config?: WorkspaceConfiguration;
   private client?: Client;
@@ -38,39 +42,47 @@ export class ChatClient implements Disposable {
   private leaveMessage: string = "";
   private requiredBadges: string[] = [];
 
-  constructor(private log: log) { }
+  constructor(private log: log) {}
 
   public initialize(context: ExtensionContext) {
     this.config = workspace.getConfiguration(Configuration.sectionIdentifier);
     this.announceBot = this.config.get<boolean>(Settings.announceBot) || true;
     this.joinMessage = this.config.get<string>(Settings.joinMessage) || "";
     this.leaveMessage = this.config.get<string>(Settings.leaveMessage) || "";
-    this.requiredBadges = this.config.get<string[]>(Settings.requiredBadges) || [];
+    this.requiredBadges =
+      this.config.get<string[]>(Settings.requiredBadges) || [];
 
-    context.subscriptions.push(workspace.onDidChangeConfiguration(this.onDidChangeConfigurationHandler, this));
+    context.subscriptions.push(
+      workspace.onDidChangeConfiguration(
+        this.onDidChangeConfigurationHandler,
+        this
+      )
+    );
   }
 
   public async connect() {
     if (keytar && this.config && !this.isConnected) {
-      const accessToken = await keytar.getPassword(KeytarKeys.service, KeytarKeys.account);
-      const login = await keytar.getPassword(KeytarKeys.service, KeytarKeys.userLogin);
-      if (accessToken && login) {
-        this.channel = this.config.get<string>(Settings.channels) || login;
-        const opts: Options = {
-          identity: {
-            username: login,
-            password: accessToken
-          },
-          channels: this.channel.split(', ').map(c => c.trim())
-        };
-        this.client = Client(opts);
-        this.client.on('connected', this.onConnectedHandler.bind(this));
-        this.client.on('message', this.onMessageHandler.bind(this));
-        this.client.on('join', this.onJoinHandler.bind(this));
-        const status = await this.client.connect();
-        this._onChatClientConnected.fire(true);
-        return status;
-      }
+      const accessToken = await keytar.getPassword(
+        KeytarKeys.service,
+        KeytarKeys.account
+      );
+      const login = await keytar.getPassword(
+        KeytarKeys.service,
+        KeytarKeys.userLogin
+      );
+      // if (accessToken && login) {
+      this.channel = "typescriptteatime"; //this.config.get<string>(Settings.channels) || login;
+      const opts: Options = {
+        channels: this.channel.split(", ").map((c) => c.trim()),
+      };
+      this.client = Client(opts);
+      this.client.on("connected", this.onConnectedHandler.bind(this));
+      this.client.on("message", this.onMessageHandler.bind(this));
+      this.client.on("join", this.onJoinHandler.bind(this));
+      const status = await this.client.connect();
+      this._onChatClientConnected.fire(true);
+      return status;
+      // }
     }
   }
 
@@ -92,17 +104,27 @@ export class ChatClient implements Disposable {
   }
 
   private get isConnected(): boolean {
-    return this.client ? this.client.readyState() === 'OPEN' : false;
+    return this.client ? this.client.readyState() === "OPEN" : false;
   }
 
   private async sendMessage(message: string) {
     if (this.isConnected && this.client) {
-      await this.client.say(this.channel, message);
+      console.log("would say:", this.channel, message);
+      // await this.client.say(this.channel, message);
     }
   }
 
-  private async onJoinHandler(channel: string, username: string, self: boolean) {
-    if (self && this.client && this.announceBot && this.joinMessage.length > 0) {
+  private async onJoinHandler(
+    channel: string,
+    username: string,
+    self: boolean
+  ) {
+    if (
+      self &&
+      this.client &&
+      this.announceBot &&
+      this.joinMessage.length > 0
+    ) {
       this.log(`Joined channel: ${channel} as ${username}`);
       await this.sendMessage(this.joinMessage);
     }
@@ -112,7 +134,12 @@ export class ChatClient implements Disposable {
     this.log(`Connected chat client to ${address} port ${port}`);
   }
 
-  private async onMessageHandler(channel: string, userState: ChatUserstate, message: string, self: boolean) {
+  private async onMessageHandler(
+    channel: string,
+    userState: ChatUserstate,
+    message: string,
+    self: boolean
+  ) {
     this.log(`Received '${message}' from ${userState["display-name"]}`);
 
     if (self) {
@@ -123,36 +150,51 @@ export class ChatClient implements Disposable {
       return;
     }
 
-    const badges = userState.badges as IBadges || {};
-    badges.follower = await API.isUserFollowingChannel(userState.id!, channel) === true ? '1' : '0';
+    const badges = (userState.badges as IBadges) || {};
+    badges.follower =
+      (await API.isUserFollowingChannel(userState.id!, channel)) === true
+        ? "1"
+        : "0";
 
     if (this.requiredBadges.length > 0 && !badges.broadcaster) {
       // Check to ensure the user has a required badge
-      const canContinue = this.requiredBadges.some(badge => badges[badge] === '1');
+      const canContinue = this.requiredBadges.some(
+        (badge) => badges[badge] === "1"
+      );
       // Bail if the user does not have the required badge
       if (!canContinue) {
-        this.log(LogLevel.Warning, `${userState.username} does not have any of the required badges to use the highlight command.`);
+        this.log(
+          LogLevel.Warning,
+          `${userState.username} does not have any of the required badges to use the highlight command.`
+        );
         return;
       }
     }
 
     message = message.toLocaleLowerCase().trim();
 
-    if (message.startsWith('!line') || message.startsWith('!highlight')) {
+    if (message.startsWith("!line") || message.startsWith("!highlight")) {
       // message = message.replace('!line', '').replace('!highlight', '').trim();
       if (message.length === 0) {
-        this.sendMessage('💡 To use the !line command, use the following format: !line <number> --or-- multiple lines: !line <start>-<end> --or-- with a comment: !line <number> <comment>');
+        this.sendMessage(
+          "💡 To use the !line command, use the following format: !line <number> --or-- multiple lines: !line <start>-<end> --or-- with a comment: !line <number> <comment>"
+        );
         return;
       }
       this._onChatClientMessageReceived.fire({
         userState,
-        message
+        message,
       });
     }
   }
 
-  private async onDidChangeConfigurationHandler(event: ConfigurationChangeEvent) {
-    if (event.affectsConfiguration(Configuration.sectionIdentifier) && this.isConnected) {
+  private async onDidChangeConfigurationHandler(
+    event: ConfigurationChangeEvent
+  ) {
+    if (
+      event.affectsConfiguration(Configuration.sectionIdentifier) &&
+      this.isConnected
+    ) {
       await this.disconnect();
       await this.connect();
     }
